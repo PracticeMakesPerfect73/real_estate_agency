f8b644a4843d4a4b93773bbc4a12831cbc549586
Alexey <practicemakesperfect73@gmail.com>
2025-04-16 22:10:27 +0900

    Добавил фильтрацию в админке

--- property/admin.py
+++ property/admin.py
@@ -7,6 +7,7 @@ class FlatAdmin(admin.ModelAdmin):
         'address', 'price','new_building', 'construction_year', 'town'
     )
     list_editable = ('new_building',)
+    list_filter = ('new_building', 'rooms_number', 'has_balcony')
     search_fields = ('town','address', 'owner')
     readonly_fields = ["created_at"]
     pass
